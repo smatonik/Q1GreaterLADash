0cbc7859a9459223bf5d542555bda041a0b4916e
smatonik <smatonik@gmail.com>
2024-10-25 11:53:13 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Outreach from './Outreach';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Outreach />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
